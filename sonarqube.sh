6dfc98a45a1e692e2f393bdcb33ac84a9bfad239
Vanessa <vantion@yahoo.com>
2022-03-03 21:48:12 -0600

    new sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,14 +1,38 @@
   #Author: Vanessa
   #Date: Feb 22 2022 
   
-  echo "We are Installing Sonarqube on CentOS7" 
-  echo su - vagrant sudo yum update -y sudo yum install java-11-openjdk-devel -y 
-  sudo yum install java-11-openjdk-devel -y 
-  cd/opt 
-  sudo yum install wget -y 
-  sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip 
-  sudo unzip /opt/sonarqube-9.3.0.51899.zip 
-  sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899 
-  cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64 ./sonar.sh start 
-  echo "Sonarqube installed and started successfully"
+ #!/bin/bash
+
+
+ #### Installation of Packages on CentOS7 ####
+
+
+ echo "Installing packages please wait..."
+
+ su - vagrant
+
+ sudo yum update -y
+
+ sudo yum install java-11-openjdk-devel -y
+
+ sudo yum install java-11-openjdk -y
+
+ cd /opt  
+
+ sudo yum install wget -y
+
+ sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+
+ sudo yum install unzip -y
+
+ sudo unzip /opt/sonarqube-9.3.0.51899.zip
+
+ sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+
+ cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
+
+ ./sonar.sh start
+
+ sudo firewall-cmd --permanent --add-port=9000/tcp
+ echo "Sonarqube installed and started successfully"
 
